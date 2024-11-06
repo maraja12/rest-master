45cd394d55d8daadebd9f0fafe86a80b6831b80f
maraja12 <marijarajic64@gmail.com>
2024-11-06 12:19:24 +0100

    Wrote sql query for creating table company

---
+++ src/main/resources/liquibase/sqlserver/setup/ddl/create-company.sql
@@ -0,0 +1,10 @@
+create table company(
+    pib int not null,
+    constraint ck_pib_limit check ( pib between 100000000 and 999999999),
+    name varchar(30) collate Latin1_General_CS_AS not null,
+    constraint ck_capitalized check (name LIKE '[A-Ž]%'),
+    address nvarchar(50) not null,
+    email varchar(30) not null unique,
+    constraint ck_email_formatting check ( email like '%_@_%._%' ),
+    primary key (pib)
+)
